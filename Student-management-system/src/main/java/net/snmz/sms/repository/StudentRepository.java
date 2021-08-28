af6a7b8af20fc8e322430aedb62c138a7737162c
Özcan SÖNMEZ <snmz9000@gmail.com>
2021-08-28 16:29:26 +0300

    Update StudentRepository
    güncellendi StudentRepository eksikler ve düzenleme yapıldı

--- Student-management-system/src/main/java/net/snmz/sms/repository/StudentRepository.java
+++ Student-management-system/src/main/java/net/snmz/sms/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package net.snmz.sms.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.snmz.sms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student,Long>{
+
 
 }
