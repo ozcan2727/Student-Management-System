588bf201dd0d526b7a0beb4fe1cd805489f4e69f
Özcan SÖNMEZ <snmz9000@gmail.com>
2021-08-28 16:33:14 +0300

    UpdateStudentServiceImp
    güncellendi StudentServiceImp düzenleme ve eksiklikler gerçekleştirildi

--- Student-management-system/src/main/java/net/snmz/sms/service/impl/StudentServiceImpl.java
+++ Student-management-system/src/main/java/net/snmz/sms/service/impl/StudentServiceImpl.java
@@ -2,14 +2,29 @@ package net.snmz.sms.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import net.snmz.sms.entity.Student;
+import net.snmz.sms.repository.StudentRepository;
 import net.snmz.sms.service.StudentService;
 
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
+	private StudentRepository studentRepository;
+	
+	
+	
+	
+	
+	
 	@Override
 	public List<Student> getAllStudents() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return studentRepository.findAll();
 	}
 }
