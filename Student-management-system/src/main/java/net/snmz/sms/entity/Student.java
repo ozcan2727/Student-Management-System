bf5767e14b9918fc9620f4d5949595f583ed7b94
Özcan SÖNMEZ <snmz9000@gmail.com>
2021-08-28 16:01:13 +0300

    StudentClass
    Entitiy class düzenlendi

--- Student-management-system/src/main/java/net/snmz/sms/entity/Student.java
+++ Student-management-system/src/main/java/net/snmz/sms/entity/Student.java
@@ -1,5 +1,30 @@
 package net.snmz.sms.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="students")
 public class Student {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="first_name",nullable = false)
+	private String firstName;
+	@Column(name = "last_name",nullable = false)
+	private String lastName;
+	@Column(name = "email",nullable = false)
+	private String email;
 }
