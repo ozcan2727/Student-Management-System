a04e9e4ebb7a513c04453ab42a22e5a46a35d024
Özcan SÖNMEZ <snmz9000@gmail.com>
2021-08-29 19:05:34 +0300

    UpdateStudentController
    UpdateStudentController

--- Student-management-system/src/main/java/net/snmz/sms/controller/StudentController.java
+++ Student-management-system/src/main/java/net/snmz/sms/controller/StudentController.java
@@ -1,8 +1,28 @@
 package net.snmz.sms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import net.snmz.sms.service.StudentService;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Controller
 public class StudentController {
-
+    @Autowired
+	private StudentService studentService;
+	
+//Öğrencileri listelemek ve model ve görünüm döndürmek için işleyici yöntemi
+//Kısacası öğrencileri  gösterecek method
+	@GetMapping("/students")
+	public String listStudets(Model model) {
+		model.addAttribute("students",studentService.getAllStudents());
+		
+		return "students";
+	}
 }
