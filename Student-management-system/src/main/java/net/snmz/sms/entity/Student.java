51eb9faa677db1b380c3d8bc15f7beed1b95ef6e
Özcan SÖNMEZ <snmz9000@gmail.com>
2021-08-29 19:28:07 +0300

    Update Student
    lombokda hata olduğu için kaldırdım

--- Student-management-system/src/main/java/net/snmz/sms/entity/Student.java
+++ Student-management-system/src/main/java/net/snmz/sms/entity/Student.java
@@ -6,25 +6,54 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name="students")
+
 public class Student {
 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name="first_name",nullable = false)
 	private String firstName;
-	@Column(name = "last_name",nullable = false)
+	@Column(name = "last_name")
 	private String lastName;
-	@Column(name = "email",nullable = false)
+	@Column(name = "email")
 	private String email;
+	public Student()
+	{
+		
+	}
+	public Student(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
 }
