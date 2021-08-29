a373a21b0e0d96085def51c6d327a22f784eaa4e
Özcan SÖNMEZ <snmz9000@gmail.com>
2021-08-29 19:12:23 +0300

    Update Repository

--- Student-management-system/src/main/java/net/snmz/sms/repository/StudentRepository.java
+++ Student-management-system/src/main/java/net/snmz/sms/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package net.snmz.sms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import net.snmz.sms.entity.Student;
 
 public interface StudentRepository extends JpaRepository<Student,Long>{
